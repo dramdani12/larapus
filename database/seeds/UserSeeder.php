a60274852e2564a961d74734530e36bda5c632ac
User <151610190@smkassalaambandung.sch.id>
2017-08-03 18:08:34 +0700

    larapuss

--- database/seeds/UserSeeder.php
+++ database/seeds/UserSeeder.php
@@ -1,7 +1,8 @@
 <?php
 
 use Illuminate\Database\Seeder;
-
+use App\Role;
+use App\User;
 class UserSeeder extends Seeder
 {
     /**
@@ -11,6 +12,30 @@ class UserSeeder extends Seeder
      */
     public function run()
     {
-        //
+        // Membuat role admin
+        $adminRole=new Role();
+        $adminRole->name="admin";
+        $adminRole->display_name="Admin";
+        $adminRole->save();
+
+        // Membuat role member
+        $memberRole=new Role();
+        $memberRole->name="member";
+        $memberRole->display_name="Member";
+        $memberRole->save();
+        // Membuat sample admin
+        $admin=new User();
+        $admin->name='Admin Larapus';
+        $admin->email='admin@gmail.com';
+        $admin->password=bcrypt('rahasia');
+        $admin->save();
+        $admin->attachRole($adminRole);
+        //Membuat sample member
+        $member=new User();
+        $member->name='Sample Member';
+        $member->email='member@gmail.com';
+        $member->password=bcrypt('rahasia');
+        $member->save();
+        $member->attachRole($memberRole);
     }
 }
